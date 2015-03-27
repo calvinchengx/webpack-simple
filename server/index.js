fd13731f77c4d1bea9283c3b5fac4b93c5d690e9
Calvin Cheng <linchuan.cheng@gmail.com>
2015-03-27 14:31:07 +0800

    Clarifying the architectural differences for static assets vs pre-generated static html pages vs isomorphic csr and ssr reactjs components

--- server/index.js
+++ server/index.js
@@ -9,11 +9,19 @@ var serverRender = require('../components/ServerRender');
 
 app.use(morgan('dev'));  // logger middleware to stdout
 
+
 var path = require('path');
+
 var cssPath = path.resolve(__dirname, '..', 'public/css');
 var jsPath = path.resolve(__dirname, '..', 'public/js');
 app.use('/css', express.static(cssPath));
 app.use('/js', express.static(jsPath));
+
+// This is where our pre-generated static pages will go to
+// TODO: update our mdToHtml script to generate standalone html content
+var blogPath = path.resolve(__dirname, '..', 'content/html');
+app.use('/', express.static(blogPath, {'extensions': ['html']}));
+
 app.get('/*', serverRender);
 
 app.listen(5000, function() {
