62ef05074bcff7a0dbc61b6106a7fd9b2fa7f480
Calvin Cheng <linchuan.cheng@gmail.com>
2015-04-02 12:28:11 +0800

    Clean up build script

--- content/build.js
+++ content/build.js
@@ -11,28 +11,13 @@ var paths = require('./paths');
 
 var blogPath = path.resolve(__dirname, '..', 'public', 'blog');
 mkdirp.sync(blogPath);
-console.log(blogPath);
 
 var posts = paths.allPosts();
-console.log(posts);
 var writePost = function(value, key, post) {
-  console.log('This is the post: ' + key);
   var file = blogPath + '/' + key + '.html';
-  console.log(file);
   var url = '/blog/' + key;
-  console.log(url);
   var dataFromUrl = staticRender(url);
-  console.log(dataFromUrl);
   fs.writeFileSync(file, dataFromUrl);
 };
-R.mapObjIndexed(writePost, posts);
 
-//for (var post in paths.allPosts()) {
-  //var file = blogPath + '/' + post + '.html';
-  //console.log(file);
-  //var url = '/blog/' + post;
-  //console.log(url);
-  //var dataFromUrl = staticRender(url);
-  //console.log(dataFromUrl);
-  //fs.writeFileSync(file, dataFromUrl);
-//}
+R.mapObjIndexed(writePost, posts);
